dc0b1494dbbbdc79f6f7a8ef349f41760e030a85
Aniket Tiwari <tiwarianiket987@gmail.com>
2025-03-01 22:29:45 +0530

    constants folder is created

---
+++ src/Constants/alumnijob.js
@@ -0,0 +1,24 @@
+const jobs = [
+    {
+      title: "Front-End Engineer",
+      location: "Navi Mumbai",
+      Company: "WalletX",
+      date: "3 Days ago",
+      alumni: "Posted by Jane Doe '$2015 • Software Engineering",
+      description:
+        "Seeking a talented engineer to join our growing team. Experience with React and Node.js required.",
+      btn: "Apply Now",
+    },
+    {
+      title: "Product Marketing Manager",
+      location: "Bandra",
+      Company: "Creative Solutions LLC",
+      date: "5 Days ago",
+      alumni: "Posted by John Smith '$2018 • Marketing",
+      description:
+        "We are looking for a creative marketing professional to drive our product strategy.",
+      btn: "Apply Now",
+    },
+  ];
+
+  export default  jobs;
--- src/Pages/Alumnijob.jsx
+++ src/Pages/Alumnijob.jsx
@@ -1,35 +1,12 @@
 import { React, useState } from "react";
 import JobApplicationForm from "./JobApplicationForm";
+import jobs from "../Constants/alumnijob";
 
 function Alumnijob() {
   const [isOpen, setIsOpen] = useState(false);
   const [isMenu, setIsMenu] = useState(false);
   const [selectedJob, setSelectedJob] = useState(null);
   const [showApplicationForm, setShowApplicationForm] = useState(false);
-
-  const jobs = [
-    {
-      title: "Front-End Engineer",
-      location: "Navi Mumbai",
-      Company: "WalletX",
-      date: "3 Days ago",
-      alumni: "Posted by Jane Doe '$2015 • Software Engineering",
-      description:
-        "Seeking a talented engineer to join our growing team. Experience with React and Node.js required.",
-      btn: "Apply Now",
-    },
-    {
-      title: "Product Marketing Manager",
-      location: "Bandra",
-      Company: "Creative Solutions LLC",
-      date: "5 Days ago",
-      alumni: "Posted by John Smith '$2018 • Marketing",
-      description:
-        "We are looking for a creative marketing professional to drive our product strategy.",
-      btn: "Apply Now",
-    },
-  ];
-
   const [searchInput, setSearchInput] = useState("");
   const filteredJobs = jobs.filter(
     (job) =>
